2b743a62e5b98e78d4fb1ea9c189caaac3aca555
David malý <maly.david.it@gmail.com>
2018-06-05 16:38:28 +0200

    Init

---
+++ Utils.FluentValidation/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils.FluentValidation
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public object SomeSpecialProperty { get; set; }
+    }
+}
--- Utils.FluentValidation/Program.cs
+++ Utils.FluentValidation/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using FluentValidation.Results;
 
 namespace Utils.FluentValidation
 {
@@ -6,7 +9,49 @@ namespace Utils.FluentValidation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person pValid = new Person()
+            {
+                FirstName = "Jan",
+                LastName = "Novák",
+                DateOfBirth = new DateTime(1993, 8, 24),
+                SomeSpecialProperty = "asdas"
+            };
+
+            Person pNotValid = new Person()
+            {
+                FirstName = "Jan",
+                LastName = "Novák",
+                DateOfBirth = new DateTime(1993, 8, 24),
+                SomeSpecialProperty = null
+            };
+
+            PersonValidator personValidator = new PersonValidator();
+
+
+            ValidationResult results = personValidator.Validate(pValid);
+            if (results.IsValid)
+            {
+                Console.WriteLine("ok");
+            }
+            else
+            {
+                Console.WriteLine(results.Errors);
+            }
+
+            ValidationResult results2 = personValidator.Validate(pNotValid);
+            if (results2.IsValid)
+            {
+                Console.WriteLine("ok");
+            }
+            else
+            {
+                results2.Errors.ToList().ForEach(i => Debug.WriteLine(i.ToString()));
+            }
+
+            // Valid only single property
+            ValidationResult results3 = ValidationExtensions.Validate(personValidator, pValid, "SomeSpecialProperty");
+            ValidationResult results4 = ValidationExtensions.Validate(personValidator, pNotValid, "SomeSpecialProperty");
+
         }
     }
 }
