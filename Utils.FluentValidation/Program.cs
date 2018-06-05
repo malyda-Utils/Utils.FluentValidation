fb11014798625e9676256d4ecc2f9b92fc0d1517
David malý <maly.david.it@gmail.com>
2018-06-05 16:31:45 +0200

    Přidejte soubory projektu.

---
+++ Utils.FluentValidation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Utils.FluentValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
